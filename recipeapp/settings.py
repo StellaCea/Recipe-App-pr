0eb1b8c21375451df673bb4bddeee7b29e3f952b
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-10-07 11:06:00 +0200

    added allowed hosts

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['recipeapp-p-221050f1a22a.herokuapp.com']
+ALLOWED_HOSTS = ['recipeapp-p-221050f1a22a.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
